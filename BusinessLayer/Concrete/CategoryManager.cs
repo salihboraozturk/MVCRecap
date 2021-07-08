5c523e64911755685577f298cad80688dce36cd9
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-08 20:11:58 +0300

    08/07/2021

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using DataAccessLayer.Concrete.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager
+    {
+        private GenericRepository<Category> repo = new GenericRepository<Category>();
+
+        public List<Category> GetAllBL()
+        {
+            return repo.List();
+        }
+        public void CategoryAddBL(Category category)
+        {
+            if (category.CategoryName == "")
+            {
+
+            }
+            else
+            {
+                repo.Insert(category);
+            }
+        }
+    }
+}
