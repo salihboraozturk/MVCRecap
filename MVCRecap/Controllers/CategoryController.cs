43d9925532ec5bbac9ca45ae30f241b8f74a712e
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-09 19:31:09 +0300

    data was pulled and displayed

--- MVCRecap/Controllers/CategoryController.cs
+++ MVCRecap/Controllers/CategoryController.cs
@@ -3,15 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLayer.Concrete;
 
 namespace MVCRecap.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+        private CategoryManager cm = new CategoryManager();
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetAllBL();
+            return View(categoryValues);
         }
     }
 }
