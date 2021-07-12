4a9819f8d0e8363846ad6dda5d8fb6d62f8a63ef
Salih Bora ÖZTÜRK <salihb26@gmail.com>
2021-07-12 19:04:24 +0300

    17/07/2021

---
+++ DataAccessLayer/Migrations/202107121556076_mg1_edit_writer_image.cs
@@ -0,0 +1,18 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mg1_edit_writer_image : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Writers", "WriterImage", c => c.String(maxLength: 250));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Writers", "WriterImage", c => c.String(maxLength: 100));
+        }
+    }
+}
--- EntityLayer/Concrete/Writer.cs
+++ EntityLayer/Concrete/Writer.cs
@@ -15,7 +15,7 @@ namespace EntityLayer.Concrete
         public string WriterName { get; set; }
         [StringLength(50)]
         public string WriterSurname{ get; set; }
-        [StringLength(100)]
+        [StringLength(250)]
         public string WriterImage { get; set; }
         [StringLength(100)]
         public string WriterAbout { get; set; }
